c50eac29a65f8ea088781af01f14c8be7c5ef034
shefiaa <shefiaanggraeni37@gmail.com>
2023-09-23 09:34:49 +0700

    Mengumpulkan Tugas Week6 CALCULATOR

---
+++ script.js
@@ -0,0 +1,23 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        displayValue = result.toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        alert("Hai, saya Shefiaa. Ini adalah kalkulator sederhana");
+        alert("Error Masukan Angka");
+        clearDisplay();
+    }
+}
